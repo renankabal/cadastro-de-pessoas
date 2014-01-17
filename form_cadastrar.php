5a70d219f57ebd9bb24d480b1594d25d6d88de56
jobino <jobino-araujo@hotmail.com>
2014-01-17 17:11:47 -0200

    Create form_cadastrar.php

---
+++ form_cadastrar.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Cadastro</title>
+    </head>
+    
+    <body bgcolor ="green">
+<?php require_once("menu.php"); ?>
+
+
+
+        <form action="cadastrar.php" method="post">
+            
+            <?php require_once("campos_pessoa.php"); ?>
+            
+            <center>
+            <input type="submit" value="Enviar" /></center>
+        </form>   
+    </body>
+  
+</html>    
+    
