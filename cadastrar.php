b7a364acdd69316ba0d8e86a80b396b8b7b3d45e
jobino <jobino-araujo@hotmail.com>
2014-01-17 18:35:08 -0200

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -12,6 +12,8 @@
     $estado = $_REQUEST["estado"];
     $observacao = $_REQUEST["observacao"]; 
     $sexo = $_REQUEST["sexo"];
+    $fone = $_REQUEST["fone"];
+    $cpf = $_REQUEST["cpf"];
     
     $sexo = null;
     if(isset($_REQUEST["sexo"])){
@@ -33,13 +35,27 @@
     if($estado == -1){
         echo "Por favor deixe de preguiça digite o seu estado!!!<br>";
         $camposValidados = false;
-    }  
-    
+    }
+    $nome = trim($nome);
     if(empty($nome)){
         echo "Por favor deixe de preguiça digite o nome!!!<br>";
         $camposValidados = false;
     }
-
+    else if(strlen($fone) !==8){
+        echo "Tamanho invalido!!<br>";
+        $camposValidados = false;
+    }
+    $cpf = trim($cpf);
+    if(empty($cpf)){
+        echo "Por favor deixe de preguiça digite o nome!!!<br>";
+        $camposValidados = false;
+    }
+    else if(strlen($cpf) !==11){
+        echo "Cpf invalido!!<br>";
+        $camposValidados = false;
+    }
+    
+    $observacao = trim($observacao);
     if(empty($observacao)){
         echo "O campo observação é obrigatorio!!!<br>";
         $camposValidados = false;
@@ -53,6 +69,8 @@
         $pessoa["aceito"] = $aceito;
         $pessoa["estado"] = $estado;
         $pessoa["observacao"] = $observacao;
+        $pessoa["cpf"] = $cpf;
+        $pessoa["fone"] = $fone;
         
         array_push($_SESSION["cadastros"], $pessoa);
     
