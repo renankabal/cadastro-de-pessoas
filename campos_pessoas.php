fb53cc60476b63b48020c5cfc3ea8c05b17f6e47
jobino <jobino-araujo@hotmail.com>
2014-01-17 17:09:55 -0200

    Create campos_pessoas.php

---
+++ campos_pessoas.php
@@ -0,0 +1,23 @@
+<center>
+<font color="white"><label>Nome :</label></font>
+<input type="text" name="nome" /><br><br>
+<input type="radio" name="sexo" value="m" checked="checked" />
+<label>Masculino</label>
+<input type="radio" name="sexo" value="f" />
+<label>Feminino</label>
+<br><br><br>
+<input type="checkbox" name="aceito" value="true" />
+<label>Aceito Contrato</label>
+<br><br>
+
+<label>Estado</label><br><br>    
+<select name="estado">
+    <option value="-1">Selecione</option>    
+    <option>Bahia</option>
+    <option>Goiás</option>    
+    <option>Amazonas</option>
+</select>
+<br><br>
+<label>Observação</label><br><br>
+<textarea name="observacao"></textarea>
+</center>
