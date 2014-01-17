f29e2ee8438de079a705f66433daca648086e636
jobino <jobino-araujo@hotmail.com>
2014-01-17 17:06:21 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+   
+   echo '<body bgcolor="orange">';
+  
+    require_once("menu.php");
+    
+    session_start();
+    
+    
+    if(isset($_SESSION["cadastros"])){
+        
+        $cadastros = $_SESSION["cadastros"];
+        
+        echo "<dl>";
+        foreach($cadastros as $pessoa){
+            $sexo = $pessoa["sexo"];
+            $aceito = $pessoa["aceito"];
+            
+        if($pessoa != null){
+            echo "<dt>Nome :" . $pessoa["nome"] . "</dt>";
+            echo '<br>';
+            echo "<dd>Sexo : "  . $pessoa["sexo"]  . "</dd>";
+            echo '<br>';
+            echo "<dd>Estado: " . $pessoa["estado"] . "</dd>";
+            echo '<br>';
+            echo "<dd>Aceita o contrato: ";
+            if($aceito){
+                echo "sim";
+            }
+            else{
+                echo "Não";
+            }
+            echo "</dd>";
+            echo '<br>';
+        }
+            
+    }
+    echo "</dl>";
+}
+else{
+    echo "Não existem pessoas cadastradas";
+}
+    
+?>
