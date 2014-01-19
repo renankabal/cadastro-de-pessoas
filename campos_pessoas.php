a6a9ce1989466101c38cc0c6d75273bf720992a2
jobino <jobino-araujo@hotmail.com>
2014-01-19 03:06:13 -0200

    Update campos_pessoas.php

--- campos_pessoas.php
+++ campos_pessoas.php
@@ -1,27 +1,32 @@
 <center>
-<font color="white"><label>Nome :</label></font>
-<input type="text" name="nome" /><br><br>
-<input type="radio" name="sexo" value="m" checked="checked" />
+<fieldset><font color="white"><label>Nome :</label></font>
+<input type="text" name="nome" /></fieldset><br><br>
+<fieldset><font color="white"><label>Fone :</label></font>
+<input type="text" name="fone" maxlength="9"/></fieldset><br><br>
+<fieldset><font color="white"><label>CPF  :</label></font>
+<input type="password" name="cpf" maxlength="14"/></fieldset><br>
+<fieldset><font color="white"><label>Site Preferido :</label></font>
+<input type="text" name="site" /></fieldset><br><br>
+<fieldset><font color="white"><label>Emaill :</label></font>
+<input type="text" name="email" /></fieldset><br><br>
+<fieldset><input type="radio" name="sexo" value="masculino" checked="checked" />
 <label>Masculino</label>
-<input type="radio" name="sexo" value="f" />
-<label>Feminino</label>
-<br><br><br>
-<font color="white"><label>Telefone :</label></font>
-<input type="text" name="fone" maxlength="8"/><br><br>
-<font color="white"><label>CPF :</label></font>
-<input type="text" name="cpf" maxlength="11"/><br><br>
+<input type="radio" name="sexo" value="feminino" />
+<label>Feminino</label></fieldset>
+<br><fieldset>
 <input type="checkbox" name="aceito" value="true" />
-<label>Aceito Contrato</label>
-<br><br>
-
-<label>Estado</label><br><br>    
+<label>Aceito Contrato</label></fieldset>
+<br><fieldset>  
 <select name="estado">
-    <option value="-1">Selecione</option>    
-    <option>Bahia</option>
-    <option>Goiás</option>    
+    <option value="-1">Estado</option>
+    <option>Bahia</option>    
+    <option>Goiás</option>
     <option>Amazonas</option>
-</select>
-<br><br>
-<label>Observação</label><br><br>
-<textarea name="observacao"></textarea>
+</select></fieldset>
+
+
+
+<fieldset><textarea name="observacao" rows="5" cols="20"></textarea></fieldset>
+<fieldset><label>Observação</label></fieldset>
+
 </center>
