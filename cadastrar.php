e2a824f559485c308bb365513966f2f75b0fe119
jobino <jobino-araujo@hotmail.com>
2014-01-19 03:04:34 -0200

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -1,83 +1,128 @@
 <?php
+    
     echo '<body bgcolor="silver">';
     
+    
+    
     require_once("menu.php");
+echo '<center>';
     session_start();
+    
     
     if(!isset($_SESSION["cadastros"])){
         $_SESSION["cadastros"] = array();
+        
+
     }
+    
     
     $nome = $_REQUEST["nome"];
-    $estado = $_REQUEST["estado"];
-    $observacao = $_REQUEST["observacao"]; 
-    $sexo = $_REQUEST["sexo"];
     $fone = $_REQUEST["fone"];
     $cpf = $_REQUEST["cpf"];
-    
+    $email = $_REQUEST["email"];
+    $site = $_REQUEST["site"];
+    $sexo = $_REQUEST["sexo"];
     $sexo = null;
     if(isset($_REQUEST["sexo"])){
-        $sexo = $_REQUEST["sexo"];
+    $sexo = $_REQUEST["sexo"];
     }
+    else{
+    echo "Selecione uma opção no sexo!";
+    $camposValidos = false;
+    }
+    $estado = $_REQUEST["estado"];
+    $observacao = $_REQUEST["observacao"];
 
+
+      
     $aceito = false;
     if(isset($_REQUEST["aceito"])){
+        
         $aceito = true;
+        
+        
     }
     
-    $camposValidados = true;
-        
+    $camposValidos = true;
+    
     if($sexo == null){
-        echo "Por favor deixe de preguiça digite o sexo!!!<br>";
-        $camposValidados = false;
+    echo "Por favor, selecione um sexo <br>";
+    $camposValidos = false;
     }
     
     if($estado == -1){
-        echo "Por favor deixe de preguiça digite o seu estado!!!<br>";
-        $camposValidados = false;
+    echo "Por favor, selecione um estado <br>";
+    $camposValidos = false;
     }
+    
+    
     $nome = trim($nome);
     if(empty($nome)){
-        echo "Por favor deixe de preguiça digite o nome!!!<br>";
-        $camposValidados = false;
+    echo "O campo nome é obrigatório!<br>";
+    $camposValidos = false;
     }
-    else if(strlen($fone) !==8){
-        echo "Tamanho invalido!!<br>";
+    
+    $fone = trim($fone);
+    if(empty($fone)){
+    echo "O campo TELEFONE é obrigatório!<br>";
+    $camposValidos = false;
+    }
+    else if(strlen($fone) !==9){
+        echo "Tamanho do TELEFONE é invalido!!<br>";
         $camposValidados = false;
     }
     $cpf = trim($cpf);
     if(empty($cpf)){
-        echo "Por favor deixe de preguiça digite o nome!!!<br>";
-        $camposValidados = false;
+    echo "O campo CPF é obrigatório!<br>";
+    $camposValidos = false;
     }
-    else if(strlen($cpf) !==11){
-        echo "Cpf invalido!!<br>";
-        $camposValidados = false;
+    else if(strlen($cpf) !==14){
+        echo "Tamanho do CPF é invalido!!<br>";
+        $camposValidos = false;
+    }
+    $email = trim($email);
+    if(empty($email)){
+    echo "O campo email é obrigatório!<br>";
+    $camposValidos = false;
+    }
+    
+    $site = trim($site);
+    if(empty($site)){
+    echo "O campo site é obrigatório!<br>";
+    $camposValidos = false;
     }
     
     $observacao = trim($observacao);
     if(empty($observacao)){
-        echo "O campo observação é obrigatorio!!!<br>";
-        $camposValidados = false;
+    echo "O campo é obrigatório,comente!<br>";
+    $camposValidos = false;
     }
+    
 
+    if($camposValidos){
+    $pessoa = array();
+    $pessoa["nome"] = $nome;
+    $pessoa["sexo"] = $sexo;
+    $pessoa["aceito"] = $aceito;
+    $pessoa["estado"] = $estado;
+    $pessoa["observacao"] = $observacao;
+    $pessoa["cpf"] = $cpf;
+    $pessoa["fone"] = $fone;
+    $pessoa["email"] = $email;
+    $pessoa["site"] = $site;
     
-    if($camposValidados){
-        $pessoa = array();
-        $pessoa["nome"] = $nome;
-        $pessoa["sexo"] = $sexo;
-        $pessoa["aceito"] = $aceito;
-        $pessoa["estado"] = $estado;
-        $pessoa["observacao"] = $observacao;
-        $pessoa["cpf"] = $cpf;
-        $pessoa["fone"] = $fone;
-        
-        array_push($_SESSION["cadastros"], $pessoa);
     
-        echo "Cadastro bem sucedido!!";
+    
+    array_push($_SESSION["cadastros"], $pessoa);
+    
+    
+    
+        echo "Cadastro bem sucedido!!<br>";
+    
+   }
+   else{
+   echo "<input type='button' onclick='history.go (-1)' value='voltar' />";
     }
-    else{
-        echo '<br>';    
-        echo "<input type='button' onclick='history.go (-1)' value='voltar' />";
-    }
+            echo '</center>';
+
 ?>
